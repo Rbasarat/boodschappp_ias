15653742ca8adad33b5cfa73cb4c43810aac1e2c
Rasjaad <rasjaad10@msn.com>
2021-01-04 17:47:10 +0100

    Added terraform vm

---
+++ variables.tf
@@ -0,0 +1,13 @@
+variable "scraper_vm_admin_username" {
+  type    = string
+  default = "niverhawk"
+}
+
+variable "ssh_public_key" {
+  type = string
+}
+
+variable "region" {
+  type    = string
+  default = "West Europe"
+}
